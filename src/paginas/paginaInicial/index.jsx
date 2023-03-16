ca53d3353b1893efde7bf000c7bbf40686514620
Cléber Severo <clebersevero1998@gmail.com>
2023-03-16 12:07:06 -0300

    jsconfig with src being the root folder to set absolute paths

--- src/paginas/paginaInicial/index.jsx
+++ src/paginas/paginaInicial/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Banner from '../../components/Banner';
-import Cabecalho from '../../components/Cabecalho';
-import { Galeria } from '../../components/Galeria';
-import Menu from '../../components/Menu';
-import Populares from '../../components/Populares';
-import Rodape from '../../components/Rodape';
+import Banner from 'components/Banner';
+import Cabecalho from 'components/Cabecalho';
+import { Galeria } from 'components/Galeria';
+import Menu from 'components/Menu';
+import Populares from 'components/Populares';
+import Rodape from 'components/Rodape';
+
 import styles from './paginaInicial.module.scss'
 
 const PaginaInicial = () => {
