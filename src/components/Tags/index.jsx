1e28d0c950a3ced7bc3bee7fc0ecc26e096e2c9f
Cléber Severo <clebersevero1998@gmail.com>
2023-03-16 12:38:18 -0300

    funciton to filter the galery by the tags using props, useState and filter method

--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import styles from './Tags.module.scss'
+import fotos from '../Galeria/fotos.json'
 
-const Tags = () => {
+const Tags = ({ tags, filtraFotos, setItens }) => {
   return (
     <div className={styles.tags}>
-        <p>Filtre por tags:</p>
-        <ul className={styles.tags__lista}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Lua</li>
-            <li>Planetas</li>
-        </ul>
+      <p>Filtre por tags:</p>
+      <ul className={styles.tags__lista}>
+
+        <li onClick={() => { setItens(fotos) }}>Todas</li>
+        {tags.map(tag => {
+          return (
+            <li key={tag} onClick={() => filtraFotos(tag)} >{tag}</li>
+          )
+        })}
+
+      </ul>
     </div>
   )
 }
